882b96c6d4fbd3f168be51ff5d7a3b5ddf5db8ec
LindaMangena <lindajazz42@gmail.com>
2022-08-12 12:09:42 +0200

    getting data from the service

--- TaskTracker/src/app/components/tasks/tasks.component.ts
+++ TaskTracker/src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Tasks';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-tasks: Task[] = TASKS;
-  constructor() { }
+tasks: Task[] = [];
+  constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
+  this.tasks =  this.taskService.getTasks();
   }
 
 }
---
+++ TaskTracker/src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from '../mock-tasks';
+import { Task } from '../Tasks'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTasks():Task[]{
+    return TASKS;
+  }
+}
